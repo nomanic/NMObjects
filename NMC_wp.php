b0842b2bf07db0d48b9d7681929a6b11868c694b
Neil Oman <nomanic99@gmail.com>
2018-03-29 02:06:34 +0100

    Add files via upload

--- NMC_wp.php
+++ NMC_wp.php
@@ -1,24 +1,23 @@
 <?php
 /**
  * Plugin Name: Nomanic Objects
- * Plugin URI: http://www.nomanic.biz/
+ * Plugin URI: http://www.nomanic.biz/NMObjects/
  * Description: A Charting and Interface Package
- * Version: 4.1
+ * Version: 4.75
  * Author: Neil Oman
  * Author URI: http://www.nomanic.biz/
- * License: Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+ * License: Envato Split License with GPL (see text file)
  *
  * Copyright 2018  Nomanic  (email : nomanic99@gmail.com)
- *
- * Read more about it here-
- * https://creativecommons.org/licenses/by-nc/3.0/
  *
  * Basically free for non-commercial use only.
  * If you wish to buy a license, please visit my website.
  */
 
 function NMC_adding_scripts() {
-wp_register_script('NMObjs', plugins_url('NMObjs.js', __FILE__));
+wp_register_script('NMKeys', plugins_url('NMKeys.js', __FILE__));
+wp_enqueue_script('NMKeys');
+wp_register_script('NMObjs', plugins_url('NMObjs.min.js', __FILE__));
 wp_enqueue_script('NMObjs');
 }
   
